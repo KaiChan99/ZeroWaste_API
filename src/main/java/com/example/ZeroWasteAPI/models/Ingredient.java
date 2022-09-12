4e3b13ea6a877fca5f1c464e21fa5e1560f09164
tio982 <t6babayemi@gmail.com>
2022-09-12 13:42:33 +0100

    getters and setters created

--- src/main/java/com/example/ZeroWasteAPI/models/Ingredient.java
+++ src/main/java/com/example/ZeroWasteAPI/models/Ingredient.java
@@ -28,4 +28,27 @@ public class Ingredient {
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public ArrayList<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(ArrayList<Recipe> recipes) {
+        this.recipes = recipes;
+    }
 }
