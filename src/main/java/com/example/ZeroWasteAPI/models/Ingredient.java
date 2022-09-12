7d28f3351033b6765e76168bf344edc59a99c77e
tio982 <t6babayemi@gmail.com>
2022-09-12 13:38:11 +0100

    created Ingredients model

--- src/main/java/com/example/ZeroWasteAPI/models/Ingredient.java
+++ src/main/java/com/example/ZeroWasteAPI/models/Ingredient.java
@@ -1,8 +1,31 @@
 package com.example.ZeroWasteAPI.models;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.ArrayList;
+
+@Entity(name = "ingredients")
 public class Ingredient {
 
+    @Column(name = "name")
     private String name;
 
+    @Id
+    @GeneratedValue
+    private long id;
+    private ArrayList<Recipe> recipes;
+
+    public Ingredient(String name, long id, ArrayList<Recipe> recipes) {
+        this.name = name;
+        this.id = id;
+        this.recipes = recipes;
+    }
+    public Ingredient(){
+
+    }
 
 }
