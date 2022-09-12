b012fdd06bcb77996084acf86dfcc2b6e3624c25
Israt Ahmed <israt.jahan237@gmail.com>
2022-09-12 12:03:17 +0100

    testing services branch

--- src/main/java/com/example/ZeroWasteAPI/services/IngredientService.java
+++ src/main/java/com/example/ZeroWasteAPI/services/IngredientService.java
@@ -1,4 +1,9 @@
 package com.example.ZeroWasteAPI.services;
 
+import com.example.ZeroWasteAPI.repositories.IngredientRepository;
+
 public class IngredientService {
+
+    IngredientRepository ingredientRepository;
+    
 }
