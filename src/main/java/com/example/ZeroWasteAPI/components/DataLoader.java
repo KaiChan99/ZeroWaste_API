0c74cfd85acdd921ca7bb4562d87661affc7a118
Shivangi <shivangi_vaghela@hotmail.co.uk>
2022-09-13 09:19:20 +0100

    added to data loader

--- src/main/java/com/example/ZeroWasteAPI/components/DataLoader.java
+++ src/main/java/com/example/ZeroWasteAPI/components/DataLoader.java
@@ -30,11 +30,11 @@ public class DataLoader implements ApplicationRunner {
         Ingredient chicken = new Ingredient(1, "chicken", ;
         ingredientRepository.save(chicken);
 
-        Recipe chickenFajitas = new Recipe(1, "Chicken Fajitas", "Step 1: Slice chicken and add to a marinade of paprika, ground coriander, " +
+        Recipe chickenFajitas = new Recipe(1, "Chicken Fajitas", "Step 1: Slice chicken and add to a marinade of paprika, ground coriander and garlic powder. Step 2:  " +
                 "", 50, 5, chicken, );
         recipeRepository.save(chickenFajitas);
 
-        Recipe creamyPestoChicken = new Recipe(2, "Creamy Pesto Chicken", "Yummy", 50, 5, chicken);
+        Recipe creamyPestoChicken = new Recipe(2, "Creamy Pesto Chicken", "Oven roasted chicken", 50, 5, chicken);
         recipeRepository.save(creamyPestoChicken);
 
         Recipe chickenParmesanPasta = new Recipe(3, "Chicken Parmesan Pasta", "Chicken Pasta", 110, 6, chicken);
@@ -45,10 +45,10 @@ public class DataLoader implements ApplicationRunner {
         Ingredient potato = new Ingredient(2,"potato", );
         ingredientRepository.save(potato);
 
-        Recipe mashedPotatoes = new Recipe(4, "Mashed potatoes", "Step 1: ", 40, 4, potato);
+        Recipe mashedPotatoes = new Recipe(4, "Mashed potatoes", "Simple potato mash that is easy to make. ", 40, 4, potato);
         recipeRepository.save(mashedPotatoes);
 
-        Recipe bakedPotato = new Recipe (5, "Baked Potato", "Step 1:", 7, 1, potato);
+        Recipe bakedPotato = new Recipe (5, "Baked Potato", "Jacket potato baked in the oven", 7, 1, potato);
         recipeRepository.save(bakedPotato);
 
         Recipe garlicRoastedPotatoes = new Recipe (6, "Garlic Roasted Potatoes", "Step 1:", 60, 8, potato);
@@ -59,16 +59,17 @@ public class DataLoader implements ApplicationRunner {
         Ingredient pepper = new Ingredient(3, "Pepper",);
         ingredientRepository.save(pepper);
 
-        Recipe stuffedPeppers = new Recipe (7, "Stuffed Peppers", "Peppers stuffed with couscous, and spices", 20, 8, pepper, rice);
+        Recipe stuffedPeppers = new Recipe (7, "Stuffed Peppers", "Peppers stuffed with rice, chicken and spices", 20, 8, pepper);
         recipeRepository.save(stuffedPeppers);
 
         //add chickenFajitas
 
         //Recipes with rice
 
-        Recipe chickenFriedRice = new Recipe (8, "Chicken Fried Rice", "Rice cooked with delicious ", 30, 7, rice, chicken);
+        Recipe chickenFriedRice = new Recipe (8, "Chicken Fried Rice", "Rice cooked with delicious chicken and peppers", 30, 7, chicken);
         recipeRepository.save(chickenFriedRice);
-        Recipe burrito = new burrito (9, "Burrito", "Tortilla filled with rice and chicken", 20, 5, rice, chicken);
+
+        Recipe burrito = new Recipe (9, "Burrito", "Tortilla filled with rice and chicken", 40, 5,  rice);
         recipeRepository.save(burrito);
 
 
